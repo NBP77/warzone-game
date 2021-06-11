f9c0e33ea0a65b7ea9012388c525da94d8bf8a1e
NBP77 <nbp77@outlook.com>
2021-06-11 09:29:49 -0400

    Getting progress on the start page

--- client/src/components/StartPage/index.js
+++ client/src/components/StartPage/index.js
@@ -2,13 +2,15 @@ import "./style.css";
 
 function StartPage() {
   return (
-    <div>
+    <div className="vertical-center">
       <div className="container-fluid">
         <div className="row">
           <h1 className="display-1 title col">Hello World</h1>
         </div>
         <div className="row">
-          <h1 className="display-1 title col">Hello World</h1>
+          <div className="display-3 btn-container col">
+            <div type="button" className="start-button">Start</div>
+          </div>
         </div>
       </div>
     </div>
