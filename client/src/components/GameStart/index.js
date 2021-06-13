04605ceb72b38587b76cb667c2d24cc1f9ab32ff
NBP77 <nbp77@outlook.com>
2021-06-13 13:27:14 -0400

    Making more progress on game card

--- client/src/components/GameStart/index.js
+++ client/src/components/GameStart/index.js
@@ -1,25 +1,39 @@
 import "./style.css";
 
-
 function GameStart() {
   return (
     <div className="vertical-center">
-      <div className="container-fluid">
-
+      <div className="container-fluid game-card">
         <div className="row">
           <h1 className="display-1 title col">Can you guess that gun?</h1>
         </div>
 
         <div className="row">
           <div className="col-4 play-btn">
-          <button>Play clip</button>
+            <button>Play clip</button>
           </div>
-          <div className="col-8 mp3-animation">
-            Put mp3 animation in 
-          </div>
+          <div className="col-8 mp3-animation">Put mp3 animation in</div>
         </div>
-        
 
+        <div className="row">
+          <form className="col answer-section">
+            <div class="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter answer"
+                name="answer"
+              />
+              <button
+                class="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+              >
+                Enter 
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
